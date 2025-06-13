f6b33a757aaa0424a8d223be762490162df609de
manuelrobertomunoz <mmroberto100@gmail.com>
2025-06-13 08:28:39 -0700

    Design system setup

---
+++ app/src/main/java/com/mmunoz/notemark/core/presentation/designsystem/Color.kt
@@ -0,0 +1,16 @@
+package com.mmunoz.notemark.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val onSurface = Color(0xFF1B1B1C)
+val onSurface12 = Color(0x1F1B1B1C)
+val onSurfaceVar = Color(0xFF535364)
+val surface = Color(0xFFEFEFF2)
+val surfaceLowest = Color(0xFFFFFFFF)
+val error = Color(0xFFE1294B)
+
+val primary = Color(0xFF5977F7)
+val primary_10 = Color(0x1A5977F7)
+
+val onPrimary = Color(0xFFFFFFFF)
+val onPrimary_12 = Color(0x1fFFFFFF)
